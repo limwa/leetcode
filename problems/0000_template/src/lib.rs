fe130dc52094608455885f1be17dde3e6e743d44
André Lima <me@limwa.pt>
2026-03-23 20:53:19 +0000

    chore: add template

---
+++ problems/0000_template/src/lib.rs
@@ -0,0 +1,23 @@
+// Paste code here
+
+struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+
+    }
+    
+    #[test]
+    fn test_2() {
+
+    }
+    
+    #[test]
+    fn test_3() {
+
+    }
+}
